b8b87cb7d356b6e59cb412168d26c4fa57cd81c2
Ahmet Faruk Ekinci <ahmetfarukekinci@gmail.com>
2024-04-24 18:48:35 +0300

    create character model

---
+++ lib/models/character.dart
@@ -0,0 +1,17 @@
+class Character {
+  final int id;
+  final String name;
+  final String species;
+  final String gender;
+  final String status;
+  final String image;
+
+  Character({
+    required this.id,
+    required this.name,
+    required this.species,
+    required this.gender,
+    required this.status,
+    required this.image,
+  });
+}
