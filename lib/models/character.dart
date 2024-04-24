6ed036856ccc9a9c59605196aacbdefd6f97ec86
Ahmet Faruk Ekinci <ahmetfarukekinci@gmail.com>
2024-04-24 19:10:44 +0300

    create character class json decoder

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -14,4 +14,25 @@ class Character {
     required this.status,
     required this.image,
   });
+
+  factory Character.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'name': String name,
+        'species': String species,
+        'status': String status,
+        'gender': String gender,
+        'image': String image,
+      } =>
+        Character(
+            id: id,
+            name: name,
+            species: species,
+            gender: gender,
+            status: status,
+            image: image),
+      _ => throw const FormatException("Failed to download character info!")
+    };
+  }
 }
