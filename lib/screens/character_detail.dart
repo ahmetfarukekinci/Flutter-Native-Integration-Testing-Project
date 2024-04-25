223373a09604674f0175d7403c26fbcbe026348f
Ahmet Faruk Ekinci <ahmetfarukekinci@gmail.com>
2024-04-25 12:53:32 +0300

    add navigation to card

--- lib/screens/character_detail.dart
+++ lib/screens/character_detail.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sc_rick_and_morty/models/character.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class CharacterDetails extends StatelessWidget {
+  final Character character;
+
+  const CharacterDetails({super.key, required this.character});
 
   @override
   Widget build(BuildContext context) {
--- lib/widgets/character_card.dart
+++ lib/widgets/character_card.dart
@@ -1,42 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:sc_rick_and_morty/models/character.dart';
+import 'package:sc_rick_and_morty/screens/character_detail.dart';
 
 class CharacterCard extends StatelessWidget {
   const CharacterCard({super.key, required this.character});
 
   final Character character;
 
+  void _onTap(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => CharacterDetails(character: character)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10.0),
-              child: Image.network(
-                character.image,
-                width: 100,
-                height: 100,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        _onTap(context);
+      },
+      child: Card(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.network(
+                  character.image,
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              character.name,
-              style: const TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
+              const SizedBox(height: 10),
+              Text(
+                character.name,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
